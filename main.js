8d4d21d9c75f66e19144717bd4284f1af5f3a8ee
Saivansh <80261246+Saivansh@users.noreply.github.com>
2022-03-07 14:59:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+menu_list_array = ["Veg Margherita Pizza",
+"Chicken Tandoori Pizza",
+"Veg Supreme Pizza",
+"Paneer Tikka Pizza",
+"Deluxe Veggie Pizza",
+"Veg Extravaganza Pizza"]:
+                    ];
+
+function getmenu(){
+var htmldata;
+function getmenu(){
+    var htmldata;
+    htmldata="(ol class='menulist'>"
+    menu_list_array.sort();
+    for (var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+
+    + menu_list_array[i] + '</li>'
+    }
+    htmldata=htmldata+"</01>"
+    document. getElementById("display menu"). innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+function additem(){
+    var htmldata;
+    var item=document.getElementById("add item"). value;
+    menu_list_array.sort();
+    htmldata=
+    "<section class='cards'>"
+   
+    for (var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class=-"card">'
+    +' <img src="images/pizzaImg.png"/>'
+    + menu_list_array[i]
+    +
+    "</div>"
+    7
+    htmldata=htmidata+"</section>"
+    document.getElementById ("display_addedmenu"). innerHTML
+    = htmldata;
+
+}
+
+function add_top(){
+    function add_top ()
+    var item=document.getElementById("add item") .value;
+    menu_list_array.push(item):
+    add_item();
+}
